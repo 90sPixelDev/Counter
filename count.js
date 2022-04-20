2f44e8cc7bf6acdf2ad8ca8ce8002c3ffe6f753b
90sPixelDev <fontlien@gmail.com>
2022-04-19 23:57:01 -0400

    learned about all powerful query selector C:

--- count.js
+++ count.js
@@ -1,38 +1,62 @@
 let counter = 0;
 
-const incBtn = document.getElementById('increase');
-const decBtn = document.getElementById('decrease');
-const resetBtn = document.getElementById('reset');
+// const incBtn = document.getElementById('increase');
+// const decBtn = document.getElementById('decrease');
+// const resetBtn = document.getElementById('reset');
 
-const counterText = document.getElementsByTagName('h2')[0];
+// const counterText = document.getElementsByTagName('h2')[0];
 
-// FIRST ATTEMPT ON MY OWN
-const increase = incBtn.addEventListener('click', () => {
-    counter++;
-    counterText.textContent = counter;
+// // FIRST ATTEMPT ON MY OWN
+// const increase = incBtn.addEventListener('click', () => {
+//     counter++;
+//     counterText.textContent = counter;
 
-    if (counter > 0) counterText.style.color = 'green';
-    if (counter === 0) counterText.style.color = 'black';
+//     if (counter > 0) counterText.style.color = 'green';
+//     if (counter === 0) counterText.style.color = 'black';
 
-    console.log(counter);
-})
-const reset = resetBtn.addEventListener('click', () => {
-    counter = 0;
-    counterText.textContent = counter;
+//     console.log(counter);
+// })
+// const reset = resetBtn.addEventListener('click', () => {
+//     counter = 0;
+//     counterText.textContent = counter;
 
-    counterText.style.color = 'black';
+//     counterText.style.color = 'black';
 
-    console.log(counter);
-})
-const decrease = decBtn.addEventListener('click', () => {
-    counter--;
-    counterText.textContent = counter;
+//     console.log(counter);
+// })
+// const decrease = decBtn.addEventListener('click', () => {
+//     counter--;
+//     counterText.textContent = counter;
 
-    if (counter < 0) counterText.style.color = 'red';
-    if (counter === 0) counterText.style.color = 'black';
+    // if (counter < 0) counterText.style.color = 'red';
+    // if (counter === 0) counterText.style.color = 'black';
 
-    console.log(counter);
-})
+//     console.log(counter);
+// })
 
 // 2nd ATTEMPT LOOKING AT HOW OTHERS DO IT
+const value = document.getElementsByTagName('h2')[0];
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach( (btn) => {
+    btn.addEventListener('click', (e) => {
+        const classes = (e.currentTarget.classList);
+        if (classes.contains('increase')) {
+            counter++;
+            value.textContent = counter;
+        }
+        if (classes.contains('reset')) {
+            counter = 0;
+            value.textContent = counter;
+        }
+        if (classes.contains('decrease')) {
+            counter--;
+            value.textContent = counter;
+        }
+
+        if (counter > 0) value.style.color = 'green';
+        if (counter < 0) value.style.color = 'red';
+        if (counter === 0) value.style.color = 'black';
+    });
+});
 
